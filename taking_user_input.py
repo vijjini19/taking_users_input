ec5c1cf295996f306a21ebd94ad3aa7db938eddf
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-06 21:39:16 +0530

    Add files via upload

---
+++ taking_user_input.py
@@ -0,0 +1,28 @@
+# a=7;b=9
+# print(a+b)  #16
+
+# a=input()
+# b=input()
+# print(type(a))
+# print(type(b))
+# print(a+b)
+
+# a=input()
+# b=input()
+# c=input()
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(a+b)
+
+# x=int(input())
+# y=int(input())
+# print(x+y)
+
+# n=float(input())
+# m=float(input())
+# print(n+m)
+
+x=str(input())
+y=str(input())
+print(x+y)
